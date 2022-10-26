6c6db5d2abd51e6bae2003bc652d0d0295d13f1c
kaling3536 <107583599+kaling3536@users.noreply.github.com>
2022-10-26 18:29:33 +0800

    Update Class19.java

--- Class19.java
+++ Class19.java
@@ -1,1 +1,24 @@
+package test;
 
+class CRectangle {
+	protected int length;
+	protected int width;
+
+	public CRectangle(int l, int w) {
+		length = l;
+		width = w;
+	}
+
+	public String toString() {
+		String str = "length=" + length + ", width=" + width;
+		str += ", area=" + length * width;
+		return str;
+	}
+}
+
+public class hw10_19 {
+	public static void main(String args[]) {
+		CRectangle rect = new CRectangle(2, 6);
+		System.out.println(rect);
+	}
+}
