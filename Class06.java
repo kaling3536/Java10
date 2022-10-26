9a083030515fb20034b6333e53efd23570181497
kaling3536 <107583599+kaling3536@users.noreply.github.com>
2022-10-26 17:52:08 +0800

    Update Class06.java

--- Class06.java
+++ Class06.java
@@ -1,1 +1,37 @@
+package test;
+
+class CTriangle {
+	protected int base;
+	protected int height;
+
+	public CTriangle(int b, int h) {
+		base = b;
+		height = h;
+	}
+
+	protected void show() {
+		System.out.println("base=" + base + ", height=" + height);
+	}
+}
+
+class CData extends CTriangle {
+	public CData(int b, int h) {
+		super(b, h);
+	}
+
+	public void area() {
+		show();
+		System.out.println("area=" + base * height / 2.0);
+	}
+}
+
+public class hw10_6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CData obj = new CData(3, 8);
+		obj.area();
+	}
+
+}
 
