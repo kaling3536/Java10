9ecd7f76d913774c515b5e439beb539fd95377ef
kaling3536 <107583599+kaling3536@users.noreply.github.com>
2022-10-26 17:56:51 +0800

    Update Class09.java

--- Class09.java
+++ Class09.java
@@ -1,1 +1,26 @@
+package test;
+
+class Cddd {
+	private int num;
+
+	public void display() {
+		System.out.println("printed from Cddd class");
+	}
+}
+
+class Cccc extends Cddd {
+	public void display() {
+		System.out.println("printed from Cccc class");
+	}
+}
+
+public class hw10_9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Cddd cc = new Cccc();
+		cc.display();
+	}
+
+}
 
