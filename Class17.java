df6952c02bea1a58f1e05de26af2cab038215fac
kaling3536 <107583599+kaling3536@users.noreply.github.com>
2022-10-26 18:28:18 +0800

    Update Class17.java

--- Class17.java
+++ Class17.java
@@ -1,1 +1,33 @@
+package test;
 
+class Car {
+	protected String owner;
+	protected String id;
+
+	public Car(String own, String s) {
+		owner = own;
+		id = s;
+	}
+
+	final void show() {
+		System.out.println("車主姓名:" + owner);
+		System.out.println("車牌號碼:" + id);
+	}
+}
+
+class CColor extends Car {
+	public String color;
+
+	public CColor(String own, String s, String col) {
+		super(own, s);
+		color = col;
+	}
+}
+
+public class hw10_17 {
+	public static void main(String args[]) {
+		CColor mycar = new CColor("Riaan", "A1-2345", "Black");
+		mycar.show();
+		System.out.println("車身顏色:" + mycar.color);
+	}
+}
