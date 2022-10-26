33987665fc7ec325d60a3b04406439305b35c742
kaling3536 <107583599+kaling3536@users.noreply.github.com>
2022-10-26 18:18:36 +0800

    Update Class13.java

--- Class13.java
+++ Class13.java
@@ -1,1 +1,29 @@
+package test;
+
+class Cqqq {
+	public int num1;
+	public int num2;
+
+	public Cqqq() {
+		num1 = 1;
+		num2 = 1;
+	}
+}
+
+class Cwww extends Cqqq {
+	public Cwww() {
+		super();
+	}
+
+	public void show() {
+		System.out.println("num1=" + num1 + ", num2=" + num2);
+	}
+}
+
+public class hw10_13 {
+	public static void main(String args[]) {
+		Cwww b1 = new Cwww();
+		b1.show();
+	}
+}
 
