994dd09841bf6308e94ca1dae1ef759374df2bdc
kaling3536 <107583599+kaling3536@users.noreply.github.com>
2022-10-26 17:22:57 +0800

    Update Class01.java

--- Class01.java
+++ Class01.java
@@ -1,1 +1,29 @@
+package test;
 
+class Caaa {
+	public int num1;
+	public int num2;
+}
+
+class Cbbb extends Caaa {
+	public void set_num(int a, int b) {
+		num1 = a;
+		num2 = b;
+	}
+
+	public void show() {
+		System.out.println("num1 = " + num1);
+		System.out.println("num2 = " + num2);
+	}
+}
+
+public class hw10_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Cbbb bb = new Cbbb();
+		bb.set_num(5, 10);
+		bb.show();
+	}
+
+}
