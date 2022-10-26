88b7912513ccf171339359c1526d8074bbf74291
kaling3536 <107583599+kaling3536@users.noreply.github.com>
2022-10-26 17:46:34 +0800

    Update Class05b.java

--- Class05b.java
+++ Class05b.java
@@ -1,1 +1,30 @@
+package test03;
 
+class Caaa {
+	private int num;
+
+	public Caaa(int n) {
+		num = n;
+	}
+
+	public int get() {
+		return num;
+	}
+}
+
+class Cbbb extends Caaa {
+	public Cbbb() {
+		super(2);
+	}
+
+	public void show() {
+		System.out.println("num=" + get());
+	}
+}
+
+public class hw10_5b {
+	public static void main(String args[]) {
+		Cbbb bb = new Cbbb();
+		bb.show();
+	}
+}
