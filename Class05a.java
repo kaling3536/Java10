6a816d25ecfe2ef1ff8f5a54b14b0179db40133a
kaling3536 <107583599+kaling3536@users.noreply.github.com>
2022-10-26 17:43:21 +0800

    Update Class05a.java

--- Class05a.java
+++ Class05a.java
@@ -1,34 +1,31 @@
-package test;
+package test02;
 
-class Caa {
+class Caaa {
 	private int num;
 
-	public Caa() {
-		this(2);          // 呼叫 Caaa(int n)建構元
+	public Caaa() {
+		this(2); // 呼叫 Caaa(int n)建構元
 	}
 
-	public Caa(int n) {
+	public Caaa(int n) {
 		num = n;
 	}
 
 	public int get() {
 		return num;
 	}
+}
 
-	class Cbbb extends Caa {
-		public void show() {
-			System.out.println("num=" + get());
-		}
+class Cbbb extends Caaa {
+	public void show() {
+		System.out.println("num=" + get());
 	}
 }
 
 public class hw10_5a {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String args[]) {
 		Cbbb bb = new Cbbb();
 		bb.show();
 	}
-
 }
 
